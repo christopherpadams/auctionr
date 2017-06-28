ff7717af0c3c3d5234057ed9124d882811b05c59
Bob Freeman PhD <rfreeman@hbs.edu>
2017-06-28 12:12:14 -0400

    bug fixes due to variable naming convention change

--- R/estimations.R
+++ R/estimations.R
@@ -26,7 +26,7 @@
 #' @export
 
 f__bid_function_fast = function(cost, num_bids, mu, alpha, gamma_1p1oa) {
-  #
+  # ?solving for the cost
   if (exp(-(num_bids-1)*(1/(mu/gamma_1p1oa)*cost)^alpha) == 0) {
     return(cost + mu/alpha*(num_bids-1)^(-1/alpha)*1/gamma_1p1oa*
              ((num_bids-1)*(gamma_1p1oa/mu*cost)^alpha)^(1/alpha-1))
@@ -45,7 +45,7 @@ vf__bid_function_fast = Vectorize(FUN = f__bid_function_fast, vectorize.args = "
 
 #' @export
 
-vf__w_integrand_z_fast = function(z, w_bid, num_bids, mu, alpha, gamma_1p1oa, param.u) {
+vf__w_integrand_z_fast = function(z, w_bid, num_bids, mu, alpha, gamma_1p1oa, param_u) {
   #
   b_z = vf__bid_function_fast(cost=z, num_bids=num_bids, mu=mu, alpha=alpha, gamma_1p1oa)
   u_z = w_bid/b_z
@@ -53,8 +53,9 @@ vf__w_integrand_z_fast = function(z, w_bid, num_bids, mu, alpha, gamma_1p1oa, pa
   vals = num_bids*alpha*(gamma_1p1oa/mu)^alpha*z^(alpha-1)*
     exp(-num_bids*(gamma_1p1oa/mu*z)^alpha)*
     1/b_z*
-    dlnorm(u_z, meanlog=(-param.u^2*1/2), sdlog = param.u) # Note: can swap for different distributions
+    dlnorm(u_z, meanlog=(-param_u^2*1/2), sdlog = param_u) # Note: can swap for different distributions
 
+  # ensuring that really large and small values do not throw errors
   vals[(gamma_1p1oa/mu)^alpha == Inf] = 0
   vals[exp(-num_bids*(gamma_1p1oa/mu*z)^alpha) == 0] = 0
 
@@ -70,7 +71,7 @@ f__funk = function(data_vec, param_u) {
                   num_bids=data_vec[2], mu=data_vec[3], alpha=data_vec[4],
                   gamma_1p1oa=data_vec[5], param_u=param_u, lower=0, upper=Inf,
                   abs.tol = 1e-10)
-  #                   rel.tol = .Machine$double.eps^0.7)
+  #                   rel_tol = .Machine$double.eps^0.7)
   if(val$message != "OK")
     stop("Integration failed.")
 
@@ -109,7 +110,7 @@ f__ll_parallel = function(x0, y, n, h_x, cl) {
   v__f_w = parApply(cl = cl, X = dat, MARGIN = 1, FUN = f__funk, param_u = u)
   v__f_y = v__f_w / v__h
 
-  return(-sum(log(v.f_y)))
+  return(-sum(log(v__f_y)))
 }
 
 
@@ -134,11 +135,11 @@ demo_auction <- function() {
   x0 = f_conv_initGuess(v__y, v__n, m__h_x)
 
   # Set up parallelization
-  cl = makeCluster(10)
-  clusterExport(cl,varlist=c("vf__bid_function_fast",
+  cl = makeCluster(4)
+  clusterExport(cl, varlist=c("vf__bid_function_fast",
                              "vf__w_integrand_z_fast",
                              "f__funk"))
-  # f__ll_parallel(x0, y = v__y, n = v__n, h_x = m__h_x, cl = cl)
+  f__ll_parallel(x0, y = v__y, n = v__n, h_x = m__h_x, cl = cl)
 
   #   Get convergence conditions
   optim_control = f_conv_conditions(x0)
@@ -151,7 +152,7 @@ demo_auction <- function() {
 
   # Return result
   return(result)
-  return("test")
+  #return("test")
 }
 
 
