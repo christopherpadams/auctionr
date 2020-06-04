d623d8b9298b5fb3027901b288e9f1991f5dd555
Ista Zahn <izahn@hbs.edu>
2020-06-04 17:06:33 -0400

    Don't run slow examples.

--- R/estimations.R
+++ R/estimations.R
@@ -55,13 +55,14 @@
 #'                               sigma = 0.2, beta = c(-1,1),
 #'                               new_x_mean= c(-1,1),
 #'                               new_x_sd = c(0.5,0.8))
-#'\donttest{res1 <- auction_model(dat1, init_param =  c(8, 2, .5, .4, .6),
+#'
+#' res1 <- auction_model(dat1, init_param =  c(8, 2, .5, .4, .6),
 #'                       num_cores = 1,
 #'                       control = list(trace=1, parscale = c(1,0.1,0.1,1,1)),
 #'                       std_err = TRUE)
 #' res1
-#'}
 #'
+#'\dontrun{ ## Examples are slow
 #' ########################################################################
 #' ## Trying out several random starting points to estimate standard errors
 #' set.seed(5)
@@ -70,15 +71,13 @@
 #'                              new_x_mean= c(-1,1),
 #'                              new_x_sd = c(0.5,0.8))
 #' ## Standard error calculation fails in the following single run
-#'\donttest{res2 <- auction_model(dat2, init_param =  c(8, 2, .5, .4, .6),
-#'                                num_cores = 1, std_err = TRUE)
+#' res2 <- auction_model(dat2, init_param =  c(8, 2, .5, .4, .6),
+#'                       num_cores = 1, std_err = TRUE)
 #' res2
-#'}
 #' ## Solving the issue with multiple runs
 #' res_list <- list()
 #' max_llik <- c()
 #' init_param0 = c(8, 2, .5, .4, .6)
-#'\donttest{
 #' set.seed(100)
 #' for (i in 1:4){
 #'    init_param1 = c(abs(init_param0[1:3]*rnorm(3) + 5*rnorm(3)),
@@ -90,9 +89,9 @@
 #'        res_list <- c(res_list, list(res3))
 #'        max_llik = c(max_llik, res3$value)
 #'    }
-#'}
 #' ## Selecting a solution with the maximum value of the likelihood
 #' res_final <- res_list[[which.max(max_llik)]]
+#' }
 #'}
 #' @seealso \code{\link{auction_generate_data}}
 #'
